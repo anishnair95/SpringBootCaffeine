9e08535cb28ffc7052d78c522f3841308b6fc4a6
Anish Nair <anishnair95@gmail.com>
2023-06-10 18:38:13 +0530

    handle rollback for checked exception

--- src/main/java/com/javatechie/crud/example/service/ProductService.java
+++ src/main/java/com/javatechie/crud/example/service/ProductService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 @CacheConfig(cacheNames = {"productCache"})
 public class ProductService implements IProductService {
 
